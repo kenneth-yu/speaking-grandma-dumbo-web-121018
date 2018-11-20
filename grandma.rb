776dc1464983f1750a861ee0266953e146c705c0
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-20 01:43:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,13 @@
 # Write a speak_to_grandma method.
 
 
-def speak_to_grandma
-  puts "HUH?! SPEAK UP, SONNY!"
+def speak_to_grandma(string)
+  if string.downcase == true
+    return "HUH?! SPEAK UP, SONNY!"
+  else if string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else 
+    return "NO, NOT SINCE 1938!"
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
